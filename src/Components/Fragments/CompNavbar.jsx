e099f2de740904c5bdd6aeb3c2684135dbd211e0
Meliana1020 <melianaa1020@gmail.com>
2024-09-20 10:56:33 +0700

    coba ganti path image

--- src/Components/Fragments/CompNavbar.jsx
+++ src/Components/Fragments/CompNavbar.jsx
@@ -1,29 +1,36 @@
-
 const CompNavbar = (props) => {
-    const { children } = props;
-    return (
-        <div className="Navigation w-full px-4 py-5 bg-white border-b border-[#3a3541]/10 flex justify-between items-center">
-            {children}
-        </div>
-    )
-}
+  const { children } = props;
+  return (
+    <div className="Navigation w-full px-4 py-5 bg-white border-b border-[#3a3541]/10 flex justify-between items-center">
+      {children}
+    </div>
+  );
+};
 const Logo = () => {
-    return (
-        <div className="flex items-center">
-           <img src="/public/logo.png" alt="Logo" className="" />
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center">
+      <img src="/logo.png" alt="Logo" className="" />
+    </div>
+  );
+};
 
 const Avatar = () => {
-    return (
-        <div className="flex items-center justify-end flex-1">
-            <p className="text-gray-500 hidden md:block mr-4">Kategori</p>
-                <img src="/public/avatar.png" alt="Profile" className="w-10 h-10 rounded hidden md:block mr-4"/>
-                <img src="/public/hamburger.png" alt="Menu" className="w-10 h-10 md:hidden mr-4"/>
-        </div>
-    )
-}
+  return (
+    <div className="flex items-center justify-end flex-1">
+      <p className="text-gray-500 hidden md:block mr-4">Kategori</p>
+      <img
+        src="/public/avatar.png"
+        alt="Profile"
+        className="w-10 h-10 rounded hidden md:block mr-4"
+      />
+      <img
+        src="/public/hamburger.png"
+        alt="Menu"
+        className="w-10 h-10 md:hidden mr-4"
+      />
+    </div>
+  );
+};
 
 CompNavbar.Logo = Logo;
 CompNavbar.Avatar = Avatar;
