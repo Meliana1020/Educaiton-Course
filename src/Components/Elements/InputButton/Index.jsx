8108dd32612c5f701f60292c614781a811d10276
Meliana1020 <melianaa1020@gmail.com>
2024-09-20 12:21:57 +0700

    .

--- src/Components/Elements/InputButton/Index.jsx
+++ src/Components/Elements/InputButton/Index.jsx
@@ -1,6 +1,6 @@
 const InputButton = () => {
     return (
-        <div className="flex flex-col md:flex-row justify-center rounded-md">
+        <div className="flex flex-wrap md:flex-row justify-center rounded-md">
             <input type="email" className="size-2.10 px-4 py-2 rounded-t-lg md:rounded-l-lg md:rounded-none border-t border-b border-l border-r border-gray-300 focus:outline-none rounded-lg "
                 placeholder="Masukkan emailmu"
                 name="email"/>
